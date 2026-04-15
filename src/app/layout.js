3fa3f14789061f91ce5442c2a864a1328aefe5a6
mdasadofficial <mdasadofficial1@gmail.com>
2026-04-16 01:33:33 +0600

    6 issue remove

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,7 +28,11 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
 
     >
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+      <head>
+
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      </head>
+
       <body className="min-h-full flex flex-col">
 
         <Navbar></Navbar>
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,32 +1,31 @@
 import React from 'react';
-
+import ImgLogo from '../../assets/logo.png'
+import Image from 'next/image';
 const Navbar = () => {
   return (
     <div className="navbar bg-base-100 shadow-sm w-[80%] mx-auto py-5 flex justify-between items-center">
 
-  {/* Left Side (Logo) */}
-  <div>
-    <a className="text-2xl font-bold">
-      <span className="text-blue-600">Keen</span>Keeper
-    </a>
-  </div>
+      {/* Left Side (Logo) */}
+      <div>
+        <Image src={ImgLogo} alt='Nav LogoImg'/>
+      </div>
 
-  {/* Right Side (Menu) */}
-  <div>
-    <ul className="menu menu-horizontal gap-4">
-      <li>
-        <a><i className="fa-solid fa-house"></i> Home</a>
-      </li>
-      <li>
-        <a><i className="fa-regular fa-clock"></i> Timeline</a>
-      </li>
-      <li>
-        <a><i className="fa-solid fa-chart-line"></i> Stats</a>
-      </li>
-    </ul>
-  </div>
+      {/* Right Side (Menu) */}
+      <div>
+        <ul className="menu menu-horizontal gap-4">
+          <li>
+            <a><i className="fa-solid fa-house"></i> Home</a>
+          </li>
+          <li>
+            <a><i className="fa-regular fa-clock"></i> Timeline</a>
+          </li>
+          <li>
+            <a><i className="fa-solid fa-chart-line"></i> Stats</a>
+          </li>
+        </ul>
+      </div>
 
-</div>
+    </div>
   );
 };
 
